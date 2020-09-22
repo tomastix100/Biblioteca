40923e6b0bf05c3012bf24e0e551fe27326bdfbd
Tomas Escobar <tomasj@unicauca.edu.co>
2020-09-22 15:24:29 -0500

    Se agrego el .gitignore y se cambio la BD

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'biblioteca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'biblioteca',
-        'USER': 'postgres',
-        'PASSWORD': 'postgresql',
-        'HOST': '127.0.0.1',
-        'DATABASE_PORT':5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
